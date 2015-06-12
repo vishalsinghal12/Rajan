e951e02205c2b52341fca442efb5bfbd72c4e814
vishal <vishal.12lucky@gmail.com>
2015-06-12 11:41:57 -0700

    first commit

---
+++ src/org/vishal/DrawingApp.java
@@ -0,0 +1,22 @@
+package org.vishal;
+
+import org.vishal.Triangle;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		Circle circle=(Circle)context.getBean("circle");
+		circle.draw();
+	System.out.println(context.getMessage("greetings", null, "default greeting	", null));
+	}
+
+}
